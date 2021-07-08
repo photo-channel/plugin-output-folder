d322a5c28d24c15e665ab4b3ec50fabca7e46c69
Stefan Berggren <nsg@nsg.cc>
2021-07-08 21:48:17 +0200

    Write file, hash filename

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
-#!/bin/bash -x
+#!/bin/bash
 
 DATA="$(jq -r .data)"
+HASH="$(echo $DATA | sha1sum | cut -d " " -f1)"
 
-echo $DATA | sha1sum | cut -d " " -f1
+echo "$DATA" | base64 -d > "/out/${HASH}.jpg"
